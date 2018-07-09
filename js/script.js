a68afdbd000832e524634641d782d5be7f9ec8f6
pozdnysheva <nataliya666@gmail.com>
2018-07-09 10:44:02 +0700

    Работа над ошибками

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var nameInput = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email-feedback]");
 var text = popup.querySelector("[name=feedback-text]");
 var form = popup.querySelector("form");
-    
+
 var isStorageSupport = true;
 var storageName = "";
 var storageEmail = "";
@@ -15,7 +15,7 @@ try {
 } catch (err) {
   isStorageSupport = false;
 }
-    
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
@@ -30,13 +30,13 @@ link.addEventListener("click", function (evt) {
     nameInput.focus();
   }
 });
-    
+
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
 });
-    
+
 form.addEventListener("submit", function (evt) {
   if (!nameInput.value || !email.value || !text.value) {
     evt.preventDefault();
@@ -50,7 +50,7 @@ form.addEventListener("submit", function (evt) {
     }
   }
 });
-    
+
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
@@ -64,14 +64,18 @@ window.addEventListener("keydown", function (evt) {
 var button1 = document.querySelector(".slide-btn-1");
 var button2 = document.querySelector(".slide-btn-2");
 var button3 = document.querySelector(".slide-btn-3");
-var bgImage = document.querySelector("body");
+var bgImage = document.querySelector(".container");
+var bgColor = document.querySelector(".index");
 var text = document.querySelectorAll(".slider-list-item");
 
 button1.addEventListener("click", function (evt) {
   evt.preventDefault();
-  bgImage.classList.remove("index-slide-2");
-  bgImage.classList.remove("index-slide-3");
-  bgImage.classList.add("index-slide-1");
+  bgColor.classList.remove("index-slide-2");
+  bgColor.classList.remove("index-slide-3");
+  bgColor.classList.add("index-slide-1");
+  bgImage.classList.remove("container-slide-2");
+  bgImage.classList.remove("container-slide-3");
+  bgImage.classList.add("container-slide-1");
   text[0].classList.remove("visually-hidden");
   text[1].classList.add("visually-hidden");
   text[2].classList.add("visually-hidden");
@@ -82,9 +86,12 @@ button1.addEventListener("click", function (evt) {
 
 button2.addEventListener("click", function (evt) {
   evt.preventDefault();
-  bgImage.classList.remove("index-slide-1");
-  bgImage.classList.remove("index-slide-3");
-  bgImage.classList.add("index-slide-2");
+  bgColor.classList.remove("index-slide-1");
+  bgColor.classList.remove("index-slide-3");
+  bgColor.classList.add("index-slide-2");
+  bgImage.classList.remove("container-slide-1");
+  bgImage.classList.remove("container-slide-3");
+  bgImage.classList.add("container-slide-2");
   text[0].classList.add("visually-hidden");
   text[1].classList.remove("visually-hidden");
   text[2].classList.add("visually-hidden");
@@ -95,9 +102,12 @@ button2.addEventListener("click", function (evt) {
 
 button3.addEventListener("click", function (evt) {
   evt.preventDefault();
-  bgImage.classList.remove("index-slide-1");
-  bgImage.classList.remove("index-slide-2");
-  bgImage.classList.add("index-slide-3");
+  bgColor.classList.remove("index-slide-1");
+  bgColor.classList.remove("index-slide-2");
+  bgColor.classList.add("index-slide-3");
+  bgImage.classList.remove("container-slide-1");
+  bgImage.classList.remove("container-slide-2");
+  bgImage.classList.add("container-slide-3");
   text[0].classList.add("visually-hidden");
   text[1].classList.add("visually-hidden");
   text[2].classList.remove("visually-hidden");
